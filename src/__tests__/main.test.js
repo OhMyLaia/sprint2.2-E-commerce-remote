6272677c75c227eca5cdb00660edeceafaf0d02c
OhMyLaia <ohmylaia@live.com>
2025-02-19 13:24:58 +0100

    first commit

---
+++ src/__tests__/main.test.js
@@ -0,0 +1,18 @@
+import { sum } from "../main.js";
+test("sum 1 + 2 must be 3", () => {
+    expect(sum(1,2)).toBe(3);
+});
+
+import { validatePassword } from "../main.js";
+
+test("validatePassword must be a function", () => {
+    expect(typeof validatePassword).toBe("function");
+});
+
+test("validatePassword must have an argument", () => {
+    expect(() => validatePassword()).toThrowError();
+});
+
+test("password must have 8 digits minimum", () => {
+    expect( () => validatePassword("abc").toThrowError());
+})
---
+++ src/main.js
@@ -0,0 +1,8 @@
+export function sum (a, b) {
+    return a + b;
+}
+export function validatePassword(input) {
+    if (!input) {
+        throw Error (`must submit a password`);
+    }
+}
